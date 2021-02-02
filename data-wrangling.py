9d62d5460f2de1764dd6a52aff171faeb1031fc2
Matt Linder <mholmeslinder@gmail.com>
2021-02-02 15:55:20 -0800

    very rough version of eval script

--- data-wrangling.py
+++ data-wrangling.py
@@ -1,6 +1,7 @@
 # Data wrangling for DAPS, RIRs, and Noise datsets for HiFi-GAN
 
 import os
+import sys
 import shutil
 # TODO unzip DAPS, RIRs, and Noise datasets
 # from zipfile import ZipFile
@@ -27,15 +28,18 @@ def dir_walk(path, ext):
                  and not name.startswith('.')]
     return file_list
 
-def make_paths(path, subpath):
-    train = path+subpath+'/train'
-    test = path+subpath+'/test'
+def make_paths(path, subpaths=[]):
+  if subpaths:
+    for subpath in subpaths:
+      newpath = path + '/' + subpath
+      if not os.path.exists(newpath):
+        os.makedirs(newpath)
+      return newpath
+  else:
+    if not os.path.exists(path):
+      os.makedirs(path)
+  return path
 
-    if not os.path.exists(train):
-        os.makedirs(train)
-
-    if not os.path.exists(test):
-        os.makedirs(test)
 
 # TODO train test split
 # create train and test folders in repo folder
@@ -44,14 +48,25 @@ def make_paths(path, subpath):
 # test_path = ''
 
 
-filenames = dir_walk(os.getcwd(), ext=('.wav', '.WAV'))
-file_ids = os.listdir('.')
+# filenames = dir_walk(os.getcwd(), ext=('.wav', '.WAV'))
+# file_ids = os.listdir('.')
 
+# for i in range(len(filenames)):
+#   # move first 200 AIRs to train/RIRS
+#   if i <=200:
+#     shutil.copyfile(filenames[i], train_path+'/IRs/'+file_ids[i])
+#   # move everything else to test/RIRS
+#   else:
+#     shutil.copyfile(filenames[i], test_path+'/IRs/'+file_ids[i])
+
+# take location of Datasets/DAPS as cli arg
+source = str(sys.argv[1])
+destination = make_paths(str(sys.argv[2]))
+filenames = dir_walk(source, ext=('.wav', '.WAV'))
+file_ids = [name for root, dir, files in os.walk(source) for name in sorted(files)]
+# For each iphone/ipad folder, take everything with "script5"
 for i in range(len(filenames)):
-  # move first 200 AIRs to train/RIRS
-  if i <=200:
-    shutil.copyfile(filenames[i], train_path+'/IRs/'+file_ids[i])
-  # move everything else to test/RIRS
-  else:
-    shutil.copyfile(filenames[i], test_path+'/IRs/'+file_ids[i])
-
+  if ("script5" in filenames[i] and "iphone" in filenames[i]) or ("script5" in filenames[i] and "ipad" in filenames[i]):
+    print(filenames[i])
+    shutil.copyfile(filenames[i], destination + '/' + file_ids[i])
+    print("moved " + file_ids[i])
